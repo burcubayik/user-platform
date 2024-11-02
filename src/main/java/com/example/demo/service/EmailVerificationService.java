8d1fe6a1196410a18973dfecb5fa27c48b670db2
Burcu Bayık <burcu.bayik98@gmail.com>
2024-11-02 16:11:28 +0300

    initial commit

--- src/main/java/com/example/demo/service/EmailVerificationService.java
+++ src/main/java/com/example/demo/service/EmailVerificationService.java
@@ -10,7 +10,7 @@ public class EmailVerificationService {
     @KafkaListener(topics = "user-registiration-topic",groupId = "verification-group")
     public void sendVerificationEmail(String verificationCode){
 
-        // E-posta gönderme işlemi (uygulamanıza entegre edilmiş bir e-posta servisi ile yapılmalı)
+
         System.out.println("Sending verification " + " with code: " + verificationCode);
 
     }
