15bf5118df4826b5faff4c1d92ecdef98a2de8a0
Burcu Bayık <burcu.bayik98@gmail.com>
2024-11-04 22:25:03 +0300

    Email sending service integrated

--- src/main/java/com/example/demo/service/EmailVerificationService.java
+++ src/main/java/com/example/demo/service/EmailVerificationService.java
@@ -1,16 +1,34 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.User;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailVerificationService {
+    private final MailSender javaMailSender;
+
+    public EmailVerificationService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
 
     @KafkaListener(topics = "user-registiration-topic",groupId = "verification-group")
     public void sendVerificationEmail(String verificationCode){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        try {
+            simpleMailMessage.setTo("");//Target email
+            simpleMailMessage.setSubject("Verify your email");
+            simpleMailMessage.setText("Verification code : " + verificationCode);
+            simpleMailMessage.setFrom(""); //Sender email
 
-
+            javaMailSender.send(simpleMailMessage);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage() + " - " + e.getCause());
+        }
         System.out.println("Sending verification " + " with code: " + verificationCode);
 
     }
